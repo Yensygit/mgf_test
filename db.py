362c7cced5bba194244b348203884d7ba276c1a3
Олег Косенок <121554768+Yensygit@users.noreply.github.com>
2023-04-21 12:10:51 +0300

    First commit

---
+++ db.py
@@ -0,0 +1,59 @@
+import time
+import psycopg2
+from psycopg2 import OperationalError
+
+
+
+create_data_table = """
+CREATE TABLE IF NOT EXISTS data (
+  id SERIAL PRIMARY KEY,
+  data TEXT NOT NULL, 
+  date text NOT NULL)"""
+
+
+
+"Создание подключения к БД"
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    file = open('log.txt', 'a', encoding='utf-8')
+    current_time = time.strftime("%m/%d/%Y %H:%M:%S")
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        file.write(f"{current_time} Успешное подключение к БД\n")
+    except OperationalError as e:
+        file.write(f"{current_time} Не удалось подключиться к БД\n")
+    return connection
+
+
+def execute_query(connection, query):
+    current_time = time.strftime("%m/%d/%Y %H:%M:%S")
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        file = open('log.txt', 'a', encoding='utf-8')
+        file.write(f"{current_time} Запрос на добавление данных в таблицу выполнен успешно\n")
+    except OperationalError as e:
+        file = open('log.txt', 'a', encoding='utf-8')
+        file.write(f"{current_time} {e} Не удалось выполнить запрос на добавление наддых в таблицу \n")
+
+
+def execute_read_query(connection, query):
+    current_time = time.strftime("%m/%d/%Y %H:%M:%S")
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        file = open('log.txt', 'a', encoding='utf-8')
+        file.write(f"{current_time} Запрос на чтение выполнен успешно\n")
+        return result
+    except OperationalError as e:
+        file = open('log.txt', 'a', encoding='utf-8')
+        file.write(f"{current_time} {e} Не удалось выполнить запрос на чтение\n")
---
+++ sender.py
@@ -0,0 +1,54 @@
+import time
+from string import ascii_lowercase
+from random import sample
+from db import *
+
+
+def main():
+    "Подключение к БД"
+    connection = create_connection("pgdb", "oleg", "123456", "192.168.1.74", "5432")
+
+    "Создание таблицы в БД если ее не существует"
+    execute_query(connection, create_data_table)
+
+
+
+    while True:
+        time.sleep(2)
+        "Получение количества записей в БД"
+        query_last_id = "SELECT id FROM data;"
+        last_id = execute_read_query(connection, query_last_id)
+        "Если записей больше 30, чистим таблицу и сбрасываем инкримент"
+        if len(last_id)>30:
+            query_delete_data = (f"DELETE FROM data")
+            query_reset_inc = (f"TRUNCATE TABLE data RESTART IDENTITY")
+            connection.autocommit = True
+            cursor = connection.cursor()
+            cursor.execute(query_delete_data)
+            cursor.execute(query_reset_inc)
+            file.write(f"Очистка таблицы прошла успешно\n")
+
+        "Запись в переменную текущего времени"
+        current_time = time.strftime("%m/%d/%Y %H:%M:%S")
+
+        "Генерация случайного слова для формирования разных данных"
+        word = ''.join(sample(ascii_lowercase, 10)).capitalize()
+
+        "Формирование корежа для передачи в запрос на добавление данных"
+        data = (word, current_time)
+
+        "Формирование запроса для добавления данных в таблицу"
+        insert_query = (f"INSERT INTO data (data, date) VALUES {data}")
+        connection.autocommit = True
+        # cursor = connection.cursor()
+
+        "Вызов метода для выполнения запроса в БД"
+        # cursor.execute(insert_query, data)
+        execute_query(connection, insert_query)
+        "Логирование работы скрипта в файл"
+        file = open('log.txt', 'a', encoding='utf-8')
+        file.write(f"Запись данных в БД прошла успешно. Добавлена запись: {word} в {current_time}\n")
+
+
+if __name__ == '__main__':
+    main()
